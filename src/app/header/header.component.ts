bbc5033404776d6404513a2bf587648843bcdffa
Mathias Keulen <mathias.keulen@external.engie.com>
2024-07-22 21:38:14 +0200

    feat/reusable header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-header',
+  selector: "app-header",
   standalone: true,
+  templateUrl: "./header.component.html",
+  styleUrl: "./header.component.css",
   imports: [],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
