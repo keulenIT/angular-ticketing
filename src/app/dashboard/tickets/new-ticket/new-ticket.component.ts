e4fae5240be04bae0e5d6ee99964ce1e650f674c
Mathias Keulen <mathias.keulen@external.engie.com>
2024-07-28 20:08:43 +0200

    feat/using host styling

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,13 +1,12 @@
-import { Component } from '@angular/core';
-import { ButtonComponent } from '../../../shared/button/button.component';
+import { Component } from "@angular/core";
+import { ButtonComponent } from "../../../shared/button/button.component";
+import { ControlComponent } from "../../../shared/control/control.component";
 
 @Component({
-  selector: 'app-new-ticket',
+  selector: "app-new-ticket",
   standalone: true,
-  imports: [ButtonComponent],
-  templateUrl: './new-ticket.component.html',
-  styleUrl: './new-ticket.component.css'
+  imports: [ButtonComponent, ControlComponent],
+  templateUrl: "./new-ticket.component.html",
+  styleUrl: "./new-ticket.component.css",
 })
-export class NewTicketComponent {
-
-}
+export class NewTicketComponent {}
